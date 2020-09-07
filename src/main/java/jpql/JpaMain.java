fa85c2486eacecca1c415194b0b3608415f6d6d9
bomingeem <bomingeem@gmail.com>
2020-09-07 22:24:44 +0900

    페치 조인

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -15,28 +15,37 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            Team team = new Team();
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
 
-            Member member = new Member();
-            member.setUsername("관리자");
-            member.setAge(10);
-            member.setTeam(team);
-            member.setType(MemberType.ADMIN);
-            member.setTeam(team);
-            em.persist(member);
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
 
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
+            em.persist(member1);
 
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-            String query = "select m from Team t join t.members m";
-            Collection result = em.createQuery(query, Collection.class)
+            String query = "select t from Team t join fetch t.members m";
+            List<Team> result = em.createQuery(query, Team.class)
                     .getResultList();
-            for (Object s : result) {
-                System.out.println("s = " + s);
-            }
+
+
             tx.commit();
 
         } catch (Exception e) {
