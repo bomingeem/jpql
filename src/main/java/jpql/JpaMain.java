09844026984e654675e40f53e4a707395170e12b
bomingeem <bomingeem@gmail.com>
2022-09-26 19:58:37 +0900

    JPQL 함수

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,9 +1,6 @@
 package jpql;
 
 import javax.persistence.*;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 import java.util.List;
 
 public class JpaMain {
@@ -29,15 +26,9 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select " +
-                    "case when m.age <= 10 then '학생요금 '" +
-                    "     when m.age >= 60 then '경로요금' " +
-                    "     else '일반요금'" +
-                    "end " +
-                    "from Member m";
+            String query = "select function('group_concat', m.username) from Member m";
 
-            String query2 = "select nullif(m.username, '관리자') from Member m";
-            List<String> result = em.createQuery(query2, String.class)
+            List<String> result = em.createQuery(query, String.class)
                     .getResultList();
 
             for (String s : result) {
